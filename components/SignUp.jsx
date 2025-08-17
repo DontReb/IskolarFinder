bb435536dad86f499e6a5e76c71da42807f89742
Liv Clarence Torres <ayentorres0503@gmail.com>
2025-08-17 21:57:27 +0800

    added react router and new routes in components.

---
+++ components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SignUp() {
+    return (
+        <div className="page">
+            <h1>Sign Up</h1>
+            <p>Placeholder</p>
+        </div>
+    );
+}
+
+export default SignUp;
