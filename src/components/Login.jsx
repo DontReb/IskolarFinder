baff61a12a49ca38651eacd2bf816e4c78a6f0ca
Liv Clarence Torres <ayentorres0503@gmail.com>
2025-08-21 20:17:54 +0800

    Added text on the open space.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,22 +2,30 @@ import React from "react";
 
 function Login() {
     return (
-        <div className="login">
-            <h1 className="loginH1">IskolarFinder</h1>
-            <p className="bigLogin">LOGIN</p>
-            <div className="loginForm">
-                <label>Username</label>
-                <input type="text" placeholder="Username" />
-                <label>Password</label>
-                <input className="passwordInput" type="password" placeholder="Password" />
+        <body>
+            <div className="loginPage">
+                <div className="login">
+                    <h1 className="loginH1">IskolarFinder</h1>
+                    <p className="bigLogin">LOGIN</p>
+                    <div className="loginForm">
+                        <label>Username</label>
+                        <input type="text" placeholder="Username" />
+                        <label>Password</label>
+                        <input className="passwordInput" type="password" placeholder="Password" />
+                    </div>
+                    <div className="forgotPasswordContainer">
+                        <p className="forgotPassword">Forgot Password?</p>
+                    </div>
+                    <button className="loginButton" type="submit">Login</button>
+                    <p className="or">or</p>
+                    <button className="SignUpButton">Sign Up</button>
+                </div>
+                <div className="rightContent">
+                    <h1>IskolarFinder</h1>
+                    <p>Start stepping toward your future with your desired scholarship. We seek opportunities for you— just one click away.</p>
+                </div>
             </div>
-            <div className="forgotPasswordContainer">
-                <p className="forgotPassword">Forgot Password?</p>
-            </div>
-            <button className="loginButton" type="submit">Login</button>
-            <p className="or">or</p>
-            <button className="SignUpButton">Sign Up</button>
-        </div>
+        </body>
     );
 }
 
